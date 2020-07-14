5523a1a73636cac56d980ec8ee0c41a494a6ccec
Hesham Ali <hesham.aly@leporis.ca>
2020-07-14 12:36:26 +0200

    converting to androidx

--- android/src/main/java/io/flutter/plugins/videoplayer/VideoPlayerPlugin.java
+++ android/src/main/java/io/flutter/plugins/videoplayer/VideoPlayerPlugin.java
@@ -7,7 +7,7 @@ import android.content.Context;
 import android.media.AudioManager;
 import android.net.Uri;
 import android.os.Build;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.view.Surface;
 import com.google.android.exoplayer2.C;
 import com.google.android.exoplayer2.ExoPlaybackException;
